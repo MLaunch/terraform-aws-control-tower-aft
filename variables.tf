05c3683d2505b85ab1121bbeb4267b0e545989d6
Matthew <80968027+MLaunch@users.noreply.github.com>
2022-05-31 16:30:33 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,48 @@
-variable "ct_management_account_id" {}
+variable "ct_management_account_id" {
+  default     = "347832306012"
+  type        = string
+  description = "Control Tower Management Account ID"
+}
 
-variable "log_archive_account_id" {}
+variable "log_archive_account_id" {
+  default     = "407466367042"
+  type        = string
+  description = "Log Archive Account ID"
+}
 
-variable "audit_account_id" {}
+variable "audit_account_id" {
+  default     = "393419545413"
+  type        = string
+  description = "Audit Account ID"
+}
 
-variable "aft_management_account_id" {}
+variable "aft_management_account_id" {
+  default     = "867293212005"
+  type        = string
+  description = "AWS Account Factory (AFT) Management Account ID"
+}
 
-variable "github_username" {}
+variable "vcs_provider" {
+  default     = "github"
+  type        = string
+  description = "Source Control Type (GitHub, GitLab, etc)"
+}
 
-variable "ct_home_region" {}
 
-variable "tf_backend_secondary_region" {}
+variable "ct_home_region" {
+  default     = "us-east-1"
+  type        = string
+  description = "Control Tower Management Account Home Region"
+}
+
+variable "tf_backend_secondary_region" {
+  default     = "us-west-2"
+  type        = string
+  description = "TF Backup Secondary Region"
+}
+
+variable "github_username" {
+  default     = "mlaunch"
+  type        = string
+  description = "GitHub User Name"
+}
